9cad9e5fb6a93b7e9caa554d90a6e83e08b9d428
Nisarga Kunder <nisarga.kunder@koerber-supplychain.com>
2025-02-25 15:11:11 +0530

    aws

---
+++ lab2-hashmap/src/main/java/org/example/student/Student.java
@@ -0,0 +1,62 @@
+package org.example.student;
+
+
+import java.util.Objects;
+
+class Student {
+        private int id;
+        private String name;
+        private int marks;
+
+    public Student(int id, String name,int marks) {
+        this.id = id;
+        this.marks = marks;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && marks == student.marks && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, marks);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
+
